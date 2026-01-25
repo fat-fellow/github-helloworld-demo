6ba68017339d6b2997d32a5710a37c980dc8655b
Mikhail Iudin <mikhail.iudin@spark.net>
2026-01-25 20:05:37 +0100

    Fix navigation approach

--- feature/repos/src/androidTest/java/mayudin/feature/repos/presentation/screen/ReposScreenTest.kt
+++ feature/repos/src/androidTest/java/mayudin/feature/repos/presentation/screen/ReposScreenTest.kt
@@ -4,7 +4,6 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.test.assertIsDisplayed
 import androidx.compose.ui.test.junit4.createComposeRule
-import androidx.compose.ui.test.onNodeWithContentDescription
 import androidx.compose.ui.test.onNodeWithTag
 import androidx.compose.ui.test.onNodeWithText
 import androidx.compose.ui.test.performTextInput
