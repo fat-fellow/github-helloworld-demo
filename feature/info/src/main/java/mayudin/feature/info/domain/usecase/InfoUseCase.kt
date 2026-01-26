4c387c123be3e50b08c00f3d414bb41861412ed9
Mikhail Iudin <mikhail.iudin@spark.net>
2026-01-26 15:05:27 +0100

    Remove usecase interfaces

--- feature/info/src/main/java/mayudin/feature/info/domain/usecase/InfoUseCase.kt
+++ feature/info/src/main/java/mayudin/feature/info/domain/usecase/InfoUseCase.kt
@@ -1,7 +1,20 @@
 package mayudin.feature.info.domain.usecase
 
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
 import mayudin.feature.info.domain.model.GitHubRepo
+import mayudin.feature.info.domain.repository.InfoRepository
+import javax.inject.Inject
+import javax.inject.Singleton
 
-interface InfoUseCase {
-    suspend operator fun invoke(params: GitHubRepo): List<String>
+@Singleton
+class InfoUseCase @Inject constructor(
+    private val reposRepository: InfoRepository,
+) {
+
+    suspend operator fun invoke(params: GitHubRepo): List<String> {
+        return withContext(Dispatchers.IO) {
+            reposRepository.getReposActivity(params)
+        }
+    }
 }
--- feature/repos/src/main/java/mayudin/feature/repos/domain/usecase/ReposUseCase.kt
+++ feature/repos/src/main/java/mayudin/feature/repos/domain/usecase/ReposUseCase.kt
@@ -1,5 +1,17 @@
 package mayudin.feature.repos.domain.usecase
 
-interface ReposUseCase {
-    suspend operator fun invoke(params: String): List<String>
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
+import mayudin.feature.repos.domain.repository.ReposRepository
+import javax.inject.Inject
+
+class ReposUseCase @Inject constructor(
+    private val reposRepository: ReposRepository,
+) {
+
+    suspend operator fun invoke(params: String): List<String> {
+        return withContext(Dispatchers.IO) {
+            reposRepository.fetchRepos(params)
+        }
+    }
 }
